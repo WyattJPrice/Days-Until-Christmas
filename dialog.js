64ad8b06fe46ccae68ac5e53cbee093195ad8223
WyattJP <79605302+WyattJPrice@users.noreply.github.com>
2022-12-11 14:04:30 -0600

    b

--- dialog.js
+++ dialog.js
@@ -1,13 +1,9 @@
 const modal = document.querySelector(".popup")
-function popup(t); {
-  if (t === open) {
-    modal.showModal();
-  } else {
-    modal.close();
-  }
+function popup(); {
+ modal.showModal();
 }
 
 modal.addEventListener('click', (event) => {
   if (event.target.class !== 'youtube') {
-      popup(close);
+modal.hide();
   }})
